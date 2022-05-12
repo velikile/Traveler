b0026b46131ea864cb2973b321f422b5bf03d8ad
Lev Veliki <levbo3@yahoo.com>
2022-05-12 21:54:56 +0300

    update build script

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-g++ -ggdb main.cpp -I/usr/local/include/SDL2 -L/usr/local/lib -Wl,-rpath,/usr/local/lib -Wl,--enable-new-dtags -lSDL2
+rm Traveler
+g++ -ggdb main.cpp -o Traveler -I/usr/local/include/SDL2 -L/usr/local/lib -Wl,-rpath,/usr/local/lib -Wl,--enable-new-dtags -lSDL2
